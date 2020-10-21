9b301382669528e17b3629b2223d4c28b02b1700
lupyuen <luppy@appkaki.com>
2020-10-21 08:38:10 +0800

    Add bitmaps

--- src/lib.rs
+++ src/lib.rs
@@ -26,28 +26,20 @@
 pub use watchface;  //  Export the Watch Face Framework
 
 use core::{
-    fmt::Write,
     ptr,
 };
-use macros::strn;
 use watchface::lvgl::mynewt::{
     result::*,
-    Strn,
 };
 use watchface::lvgl::{
     self,
     core::obj,
-    themes::theme,
-    widgets::label,
+    widgets::img,
 };
 use watchface::{
-    BluetoothState,
-    String,
     WatchFace,
     WatchFaceState,
     WatchFaceTime,
-    new_string,
-    to_strn,
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -55,15 +47,26 @@ use watchface::{
 
 /// Barebones Watch Face with no frills
 pub struct HandDrawnWatchFace {
-    /// Label for Time: "12:34"
-    pub time_label:      lvgl::Ptr,
-    /// Label for Date: "MON 22 MAY 2020"
-    pub date_label:      lvgl::Ptr,
-    /// Label for Bluetooth State (Bluetooth Icon)
-    pub bluetooth_label: lvgl::Ptr,
-    /// Label for Power Indicator (Charging & Battery)
-    pub power_label:     lvgl::Ptr,
+    /// Bitmaps for the 10 digits
+    bitmaps: [img::lv_img_dsc_t; 4],  //  TODO: Change to 10
+    /// Image at top left
+    top_left_image: lvgl::Ptr,
+    /// Image at top right
+    top_right_image: lvgl::Ptr,
+    /// Image at bottom left
+    bottom_left_image: lvgl::Ptr,
+    /// Image at bottom right
+    bottom_right_image: lvgl::Ptr,
 }
+
+/// Width of each image and bitmap
+const IMAGE_WIDTH: u32  = 80;
+
+/// Height of each image and bitmap
+const IMAGE_HEIGHT: u32 = 100;
+
+/// 2 bytes per pixel, in RGB565 format
+const BYTES_PER_PIXEL: u32 = 2;
 
 impl WatchFace for HandDrawnWatchFace {
 
@@ -75,59 +78,64 @@ impl WatchFace for HandDrawnWatchFace {
         //  Get the active screen
         let screen = watchface::get_active_screen();
 
-        //  Get the large title font for showing the time
-        let title_font = theme::get_font_title() ? ;
+        //  Compose the image header
+        let mut header = img::lv_img_header_t::default();
+        header.set_cf(img::LV_IMG_CF_TRUE_COLOR);  //  Color Format
+        header.set_w(IMAGE_WIDTH);                 //  Width
+        header.set_h(IMAGE_HEIGHT);                //  Height
+
+        //  Compute the image size
+        let data_size = IMAGE_WIDTH * IMAGE_HEIGHT * BYTES_PER_PIXEL;
+
+        //  Load the bitmaps
+        let mut bitmaps = [
+            img::lv_img_dsc_t { data: include_bytes!("../bitmaps/0.bin") as *const u8, header, data_size },
+            img::lv_img_dsc_t { data: include_bytes!("../bitmaps/1.bin") as *const u8, header, data_size },
+            img::lv_img_dsc_t { data: include_bytes!("../bitmaps/2.bin") as *const u8, header, data_size },
+            img::lv_img_dsc_t { data: include_bytes!("../bitmaps/3.bin") as *const u8, header, data_size },
+        ];
 
         //  Create the widgets
         let watch_face = Self {
-            //  Create a Label for Time: "00:00"
-            time_label: {
-                let lbl = label::create(screen, ptr::null()) ? ;  //  `?` will terminate the function in case of error
-                label::set_long_mode(lbl, label::LV_LABEL_LONG_BREAK) ? ;
-                label::set_text(     lbl, strn!("00:00")) ? ;     //  strn creates a null-terminated string
-                obj::set_width(      lbl, 240) ? ;
-                obj::set_height(     lbl, 200) ? ;
-                label::set_align(    lbl, label::LV_LABEL_ALIGN_CENTER) ? ;
-                obj::align(          lbl, screen, obj::LV_ALIGN_CENTER, 0, -30) ? ;
-                obj::set_style_local_text_font(lbl, label::LV_LABEL_PART_MAIN, obj::LV_STATE_DEFAULT, title_font) ? ;  //  Use large title font
-                lbl  //  Return the label as time_label
+            //  Set the loaded bitmaps
+            bitmaps,
+
+            //  Create the top left image
+            top_left_image: {
+                let image = img::create(screen, ptr::null()) ? ;   //  `?` will terminate the function in case of error
+                let bitmap: *mut img::lv_img_dsc_t = &mut bitmaps[0];  //  Fetch bitmap for "0"
+                img::set_src(image, bitmap as *const core::ffi::c_void) ? ;  //  Set top left image to "0"
+                obj::set_pos(image, 40, 20) ? ;  //  Top left
+                image  //  Return the image as top_left_image
             },
 
-            //  Create a Label for Date: "MON 22 MAY 2020"
-            date_label: {
-                let lbl = label::create(screen, ptr::null()) ? ;
-                label::set_long_mode(lbl, label::LV_LABEL_LONG_BREAK) ? ;
-                obj::set_width(      lbl, 200) ? ;
-                obj::set_height(     lbl, 200) ? ;
-                label::set_text(     lbl, strn!("")) ? ;  //  strn creates a null-terminated string
-                label::set_align(    lbl, label::LV_LABEL_ALIGN_CENTER) ? ;
-                obj::align(          lbl, screen, obj::LV_ALIGN_CENTER, 0, 40) ? ;
-                lbl  //  Return the label as date_label
+            //  Create the top right image
+            top_right_image: {
+                let image = img::create(screen, ptr::null()) ? ;
+                let bitmap: *mut img::lv_img_dsc_t = &mut bitmaps[1];  //  Fetch bitmap for "0"
+                img::set_src(image, bitmap as *const core::ffi::c_void) ? ;                //  Set top right image to "1"
+                obj::set_pos(image, 120, 20) ? ;  //  Top right
+                image  //  Return the image as top_right_image
             },
 
-            //  Create a Label for Bluetooth State
-            bluetooth_label: {
-                let lbl = label::create(screen, ptr::null()) ? ;
-                obj::set_width(     lbl, 50) ? ;
-                obj::set_height(    lbl, 80) ? ;
-                label::set_text(    lbl, strn!("")) ? ;  //  strn creates a null-terminated string
-                label::set_recolor( lbl, true) ? ;
-                label::set_align(   lbl, label::LV_LABEL_ALIGN_LEFT) ? ;
-                obj::align(         lbl, screen, obj::LV_ALIGN_IN_TOP_LEFT, 0, 0) ? ;
-                lbl  //  Return the label as bluetooth_label
+            //  Create the bottom left image
+            bottom_left_image: {
+                let image = img::create(screen, ptr::null()) ? ;
+                let bitmap: *mut img::lv_img_dsc_t = &mut bitmaps[2];  //  Fetch bitmap for "0"
+                img::set_src(image, bitmap as *const core::ffi::c_void) ? ;                //  Set bottom left image to "2"
+                obj::set_pos(image, 40, 120) ? ;  //  Bottom left
+                image  //  Return the image as bottom_left_image
             },
 
-            //  Create a Label for Power Indicator
-            power_label: {
-                let lbl = label::create(screen, ptr::null()) ? ;
-                obj::set_width(    lbl, 80) ? ;
-                obj::set_height(   lbl, 20) ? ;
-                label::set_text(   lbl, strn!("")) ? ;  //  strn creates a null-terminated string
-                label::set_recolor(lbl, true) ? ;
-                label::set_align(  lbl, label::LV_LABEL_ALIGN_RIGHT) ? ;
-                obj::align(        lbl, screen, obj::LV_ALIGN_IN_TOP_RIGHT, 0, 0) ? ;
-                lbl  //  Return the label as power_label
+            //  Create the bottom right image
+            bottom_right_image: {
+                let image = img::create(screen, ptr::null()) ? ;
+                let bitmap: *mut img::lv_img_dsc_t = &mut bitmaps[3];  //  Fetch bitmap for "0"
+                img::set_src(image, bitmap as *const core::ffi::c_void) ? ;                //  Set bottom right image to "3"
+                obj::set_pos(image, 120, 120) ? ;  //  Bottom right
+                image  //  Return the image as bottom_right_image
             },
+            
         };
         //  Return the watch face
         Ok(watch_face)
@@ -136,16 +144,8 @@ impl WatchFace for HandDrawnWatchFace {
     ///////////////////////////////////////////////////////////////////////////////
     //  Update Watch Face
 
-    /// Update the widgets in the Watch Face with the current state
+    /// Update the widgets in the Watch Face with the current time
     fn update(&self, state: &WatchFaceState) -> MynewtResult<()> {
-        //  Populate the Time and Date Labels
-        self.update_date_time(state) ? ;
-
-        //  Populate the Bluetooth Label
-        self.update_bluetooth(state) ? ;
-
-        //  Populate the Power Label
-        self.update_power(state) ? ;
         Ok(())
     }
 }
@@ -155,182 +155,4 @@ impl HandDrawnWatchFace {
     ///////////////////////////////////////////////////////////////////////////////
     //  Update Watch Face
 
-    /// Populate the Time and Date Labels with the time and date
-    fn update_date_time(&self, state: &WatchFaceState) -> MynewtResult<()> {
-        //  Format the time as "12:34" and set the label
-        let mut buf = new_string();
-        write!(
-            &mut buf,         //  Write the formatted text
-            "{:02}:{:02}\0",  //  Must terminate Rust strings with null
-            state.time.hour,
-            state.time.minute
-        ).expect("time fail");
-
-        label::set_text(      //  Set the label
-            self.time_label, 
-            &to_strn(&buf)
-        ) ? ;
-
-        //  Get the short day name and short month name
-        let day   = get_day_name(&state.time);
-        let month = get_month_name(&state.time);
-
-        //  Format the date as "MON 22 MAY 2020" and set the label
-        let mut buf = new_string();
-        write!(
-            &mut buf,           //  Write the formatted text
-            "{} {} {} {}\n\0",  //  Must terminate Rust strings with null
-            day,
-            state.time.day,
-            month,
-            state.time.year
-        ).expect("date fail");
-
-        label::set_text(        //  Set the label
-            self.date_label, 
-            &to_strn(&buf)
-        ) ? ;
-        Ok(())
-    }    
-    
-    /// Populate the Bluetooth Label with the Bluetooth State (Bluetooth Icon)
-    fn update_bluetooth(&self, state: &WatchFaceState) -> MynewtResult<()> {
-        if state.bluetooth == BluetoothState::BLUETOOTH_STATE_DISCONNECTED {
-            //  If Bluetooth is disconnected, leave the label empty
-            label::set_text(
-                self.bluetooth_label, 
-                strn!("")
-            ) ? ;
-        } else {
-            //  Compute the color of the Bluetooth icon
-            let color = 
-                match &state.bluetooth {
-                    BluetoothState::BLUETOOTH_STATE_INACTIVE     => "#000000",  //  Black
-                    BluetoothState::BLUETOOTH_STATE_ADVERTISING  => "#0000ff",  //  Blue
-                    BluetoothState::BLUETOOTH_STATE_DISCONNECTED => "#ff0000",  //  Red
-                    BluetoothState::BLUETOOTH_STATE_CONNECTED    => "#00ff00",  //  Green
-                };
-
-            //  Format the Bluetooth status and set the label
-            let mut buf = new_string();
-            write!(
-                &mut buf,              //  Write the formatted text
-                "{} \u{F293}#\0",      //  LV_SYMBOL_BLUETOOTH. Must terminate Rust strings with null.
-                color
-            ).expect("bt fail");
-
-            label::set_text(           //  Set the label
-                self.bluetooth_label, 
-                &to_strn(&buf)
-            ) ? ;
-        }
-        Ok(())
-    }
-
-    /// Populate the Power Label with the Power Indicator (Charging & Battery)
-    fn update_power(&self, state: &WatchFaceState) -> MynewtResult<()> {
-        //  Get the active screen
-        let screen = watchface::get_active_screen();
-
-        //  Compute the percentage power
-        let percentage = convert_battery_voltage(state.millivolts);
-
-        //  Compute the colour for the charging symbol
-        let color =                                                     //  Charging color
-            if percentage <= 20                        { "#f2495c" }    //  Low Battery
-            else if state.powered && !(state.charging) { "#73bf69" }    //  Full Battery
-            else                                       { "#fade2a" };   //  Mid Battery
-
-        let symbol =                         //  Charging symbol
-            if state.powered { "\u{F0E7}" }  //  LV_SYMBOL_CHARGE
-            else             { " " };
-
-        //  Format the Power Indicator and set the label
-        let mut buf = new_string();
-        write!(
-            &mut buf,                    //  Write the formatted text
-            "{} {}%{}#\nRUST ({}mV)\0",  //  Must terminate Rust strings with null
-            color,
-            percentage,
-            symbol,
-            state.millivolts
-        ).expect("batt fail");
-
-        label::set_text(
-            self.power_label, 
-            &to_strn(&buf)
-        ) ? ; 
-        obj::align(
-            self.power_label, screen, 
-            obj::LV_ALIGN_IN_TOP_RIGHT, 0, 0
-        ) ? ;
-        Ok(())
-    }
-
 }
-
-///////////////////////////////////////////////////////////////////////////////
-//  Date Time Functions
-
-/// Get month short name
-fn get_month_name(time: &WatchFaceTime) -> String {
-    String::from(
-        match time.month {
-            1  => "JAN",
-            2  => "FEB",
-            3  => "MAR",
-            4  => "APR",
-            5  => "MAY",
-            6  => "JUN",
-            7  => "JUL",
-            8  => "AUG",
-            9  => "SEP",
-            10 => "OCT",
-            11 => "NOV",
-            12 => "DEC",
-            _  => "???",
-        }    
-    )
-}
-
-/// Get day short name
-fn get_day_name(time: & WatchFaceTime) -> String {
-    String::from(
-        match time.day_of_week {
-            0  => "SUN",
-            1  => "MON",
-            2  => "TUE",
-            3  => "WED",
-            4  => "THU",
-            5  => "FRI",
-            6  => "SAT",
-            _  => "???",
-        }
-    )
-}
-
-///////////////////////////////////////////////////////////////////////////////
-//  Battery Functions
-
-/// Convert battery voltage to percentage
-fn convert_battery_voltage(_voltage: u32) -> i32 {
-    50  //  TODO
-}
-
-/*
-static lv_img_dsc_t <name>;
-const uint8_t <name>_map[] = {0x00,0x00,0x00...
-...0x00,0x00,0x00
-};
-
- <name>.header.always_zero = 0; //Initialization
- <name>.header.w = <picture_X>;                     // Setting the Width (or) Horizontal length of the image (number of px)
- <name>.header.h = <picture_Y>;                     // Setting the Height (or) vertical length of the image (number of px)
- <name>.data_size = <Hr_length> * <Vr_length> * LV_COLOR_SIZE / 8; //Allocation of memory for the image
- <name>.header.cf = LV_IMG_CF_TRUE_COLOR; // Sets the color scheme for the image
- <name>.data = <name>_map;                // Maps the Image data to the Array
- lv_obj_t *img_src = lv_img_create(lv_scr_act(), NULL);  // Create an image object
- lv_img_set_src(img_src, &<name>);        // Set the created file as image (<name>)
- 
- lv_obj_set_pos(img_src, <x_pos, <y_pos>); // <x_pos>, <y_pos> are the coordinates of the cartesian plane
-*/
